c87ae3a8657a75e02766c4442026a4a38bd4c786
Kolade-Kima <afolabikolade11@gmail.com>
2022-06-21 04:48:58 +0000

    advnced task commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,32 @@
 #include "main.h"
+
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- *
- * Return: new string.
- */
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
+	char *result = haystack, *fneedle = needle;
+
 	while (*haystack)
 	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		while (*needle)
 		{
-			return (haystack);
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
 		}
-		else
+		if (!*needle)
 		{
-			haystack++;
+			return (result);
 		}
+		needle = fneedle;
+		result++;
+		haystack = result;
 	}
 	return (0);
-}
-/**
- * coincidence - define if the string b is inside a.
- * @a: source string
- * @b: string to be searched
- *
- * Return: 1 if there is coincidence, otherwise 0.
- */
-int coincidence(char *a, char *b)
-{
-	while (*b && *b == *a)
-	{
-		b++;
-		a++;
-	}
-
-	if (*b == '\0')
-		return (1);
-	else
-		return (0);
 }
