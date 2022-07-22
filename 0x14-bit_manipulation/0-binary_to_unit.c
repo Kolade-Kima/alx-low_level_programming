cead9dc6cbe27ab8212ef0b6686135f9bdecb013
Kolade-Kima <afolabikolade11@gmail.com>
2022-07-22 05:11:27 +0000

    all commits

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,67 +1,47 @@
-#include "main.h"
-#include "stdlib.h"
-/**
- *_pow_recursion - returns the value of x raised to the power of y.
- *@x: parameter integer
- *@y: parameter integer
- *Return: pow number
- */
-int _pow_recursion(int x, int y)
-{
-  if (y > 0)
-    return (_pow_recursion(x, y - 1) * x);
-  else if (y == 0)
-    return (1);
-  else
-    return (-1);
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
 
 /**
- *_strlen_recursion - returns the length of a string
- *@s: pointer parameter character
- *Return: count size
- */
-int _strlen_recursion(const char *s)
-{
-  int count = 0;
-
-  if (*s)
-    {
-      count++;
-      return (count + _strlen_recursion(s + 1));
-    }
-  else
-    {
-      return (count);
-    }
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: is pointing to a string of 0 and 1 chars
- * Return: the converted number
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: The binary string to converts
+ *
+ * Return: The positive number converted from a binary
  */
 unsigned int binary_to_uint(const char *b)
 {
-  int i = 0, len = 0;
-  unsigned int number = 0;
+  unsigned int len = 0, count = 0, sum = 0;
 
-  if (!b)
+  if (b == NULL)
     return (0);
 
-  len = _strlen_recursion(b) - 1;
-
-  while (b[len])
+  len = _strlen(b);
+  while (len--)
     {
-      if (b[len] == '0')
-	number += 0;
-      else if (b[len] == '1')
-	number += _pow_recursion(2, i);
-      else
+      if (b[len] != 48 && b[len] != 49)
 	return (0);
-      len--;
-      i++;
+
+      if (b[len] == 49)
+	sum += 1 << count;
+
+      count++;
     }
 
-  return (number);
+  return (sum);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+  int c = 0;
+
+  while (s[c])
+    c++;
+
+  return (c);
 }
